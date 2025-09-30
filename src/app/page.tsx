e703328e96a6e94a066cf363a77474addcd8ed67
Santosh Kanojiya <kanojiyas780@gmail.com>
2025-09-30 14:57:46 +0000

    Reduce little more

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default async function Home() {
 
   return (
     <div className="flex flex-col gap-16 md:gap-24">
-      <section className="relative h-[50vh] w-full text-white md:h-[60vh]">
+      <section className="relative h-[40vh] w-full text-white md:h-[50vh]">
         <Image
             src={heroImageUrl}
             alt="A dynamic anime poster for the hero section."
