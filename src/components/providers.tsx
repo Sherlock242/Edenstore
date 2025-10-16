fb141aefb500956cdaaf658c7ee62841efc07284
Santosh Kanojiya <kanojiyas780@gmail.com>
2025-10-16 20:42:23 +0000

    This showing

--- src/components/providers.tsx
+++
@@ -1,7 +0,0 @@
-// This file is no longer used and can be removed. 
-// The providers have been moved to `client-providers.tsx`.
-import React, { type ReactNode } from 'react';
-
-export function AppProviders({ children }: { children: ReactNode }) {
-  return <>{children}</>;
-}
