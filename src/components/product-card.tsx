459c41e91afa35ecc0508990ab2a1e65b3f2f334
Santosh Kanojiya <kanojiyas780@gmail.com>
2025-10-27 21:14:27 +0000

    Words shouldn't be stick to edegs

--- src/components/product-card.tsx
+++ src/components/product-card.tsx
@@ -57,7 +57,7 @@ export function ProductCard({ product }: ProductCardProps) {
             )}
           </Link>
         </div>
-        <div className="pt-2">
+        <div className="pt-2 px-1">
           <Link href={`/products/${product.id}`}>
             <h3 className="font-semibold truncate text-base">{product.name}</h3>
           </Link>
